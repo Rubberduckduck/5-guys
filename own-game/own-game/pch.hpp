dff63047066a0fe3d2eabf92a04062489b1533b4
Rubberduckduck <bryanlim805@gmail.com>
2024-04-10 16:47:49 +0800

    create own game sol

---
+++ own-game/own-game/pch.hpp
@@ -0,0 +1,28 @@
+﻿/*****************************************************************************************************************
+@file         pch.hpp
+@project      MONKEE MANIA
+@author       Ho Shu Hng (100%)
+@co-authors
+@brief        This file is the pre-compiled header file for all the C++ libraries that we would require
+
+All content � 2024 DigiPen Institute of Technology Singapore, all rights reserved.
+******************************************************************************************************************/
+
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <crtdbg.h> // To check for memory leaks
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+#include "GameStateList.hpp"
+#include "AEEngine.h"
+
+#endif //PCH_H
